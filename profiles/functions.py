72d623cda5e042f40125e1b81bbd3492ee1b8ed1
anvark786 <mohammedanvark@gmail.com>
2023-12-04 23:55:04 +0530

    up

--- profiles/functions.py
+++ profiles/functions.py
@@ -14,10 +14,11 @@ def get_matching_profiles(profile_pk):
         ~Q(pk=profile_pk),
         Q(age__gte=preference.age_min) &  # Minimum age
         Q(age__lte=preference.age_max) & # Maximum age
-        Q(user__gender=preference.preferred_gender) | # Preferred gender
-        Q(address__district=preference.preferred_district) | Q(address__district='')| # Preferred district or any district
-        Q(address__street=preference.street) | Q(address__street='')| # Preferred street or any street
-        Q(address__location=preference.location) | Q(address__location='') # Preferred location or any location
+        Q(user__gender=preference.preferred_gender) 
+        # | # Preferred gender
+        # Q(address__district=preference.preferred_district) | Q(address__district='')| # Preferred district or any district
+        # Q(address__street=preference.street) | Q(address__street='')| # Preferred street or any street
+        # Q(address__location=preference.location) | Q(address__location='') # Preferred location or any location
     )
 
     return matching_profiles
